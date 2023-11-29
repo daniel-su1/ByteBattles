1e97d57d083fcafc078c7d30ff08f413e22aa53a
ey2zheng <ey2zheng@uwaterloo.ca>
2023-11-28 22:05:15 -0500

    start cmd input

--- main.cc
+++ main.cc
@@ -3,9 +3,34 @@
 #include <memory>
 #include "link.h"
 #include "data.h"
-#include "virus.h"
+#include "virus.h" // todo: remove all but gameboard
+#include "gameboard.h"
 
 int main() {
+    string cmd;
+    GameBoard gb;
+    int moves = 0;
+
+    // You will need to make changes this code.
+
+    while (true) {
+        cin >> cmd;
+        if (cmd == "new") {
+        int n;
+        cin >> n;
+        }
+        else if (cmd == "init") {
+        cout << g;
+        }
+        else if (cmd == "game") {
+        cin >> moves;
+        }
+        else if (cmd == "switch") {
+        int r = 0, c = 0;
+        cin >> r >> c;
+        }
+    }
+
     char s;
     std::vector<std::unique_ptr<Link>> links;
     while (std::cin >> s) {
