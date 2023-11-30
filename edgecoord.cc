4d7526a271a5d5c84e3df68891575e0e79362e07
Christina Li <cyli@uwaterloo.ca>
2023-11-30 14:49:33 -0500

    finished init

--- edgecoord.cc
+++ edgecoord.cc
@@ -1,3 +1,19 @@
 #include "edgecoord.h"
 
 EdgeCoord::EdgeCoord(Coords coords, Player &owner, string displayName): SpecialCoord(coords, owner, displayName) {}
+
+void EdgeCoord::attachPlayer(Player *p) {
+    owner = p;
+}
+
+Player& EdgeCoord::getOwner(){
+    return *owner;
+}
+
+string EdgeCoord::getDisplayName() {
+    return displayName;
+}
+
+vector<AbilityCard*>& EdgeCoord::getAppliedAbilities() {
+    return appliedAbilites; 
+}
--- serverport.cc
+++ serverport.cc
@@ -1,3 +1,19 @@
 #include "serverport.h"
 
 ServerPort::ServerPort(Coords coords, Player &owner, string displayName): SpecialCoord(coords, owner, displayName) {}
+
+void ServerPort::attachPlayer(Player *p) {
+    owner = p;
+}
+
+Player& ServerPort::getOwner(){
+    return *owner;
+}
+
+string ServerPort::getDisplayName() {
+    return displayName;
+}
+
+vector<AbilityCard*>& ServerPort::getAppliedAbilities() {
+    return appliedAbilites; 
+}
