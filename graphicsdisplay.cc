c45963d3e6a9ac4f0de8d52d94bc10b515f33629
Daniel Su <d2su@uwaterloo.ca>
2023-12-01 22:15:48 -0500

    fixed spacing

--- graphicsdisplay.cc
+++ graphicsdisplay.cc
@@ -10,8 +10,11 @@ GraphicsDisplay::~GraphicsDisplay() {}
 void GraphicsDisplay::notify(Link &link) {
     int x = link.getCurrCoords().getX();
     int y = link.getCurrCoords().getY();
+    int text_x = 62.5 * x + 23;
+    int text_y = 62.5 * y + (62.5 / 2) + 12;
+    string displayName = link.getDisplayName();
     theDisplay->fillRectangle(62.5 * x, 62.5 * y, 62.5, 62.5, Xwindow::Black);
-    theDisplay->drawString(62.5 * x, 62.5 * y, link.getDisplayName());
+    theDisplay->drawString(text_x, text_y, displayName);
  }
 
 void GraphicsDisplay::init(GameBoard &gb) {
